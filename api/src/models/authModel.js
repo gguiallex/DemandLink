720dc259218ae07bd524ea749db4828f13391801
Guilherme Cunha <alexandregui013@gmail.com>
2025-01-09 03:42:11 -0300

    arrumando erro de nomeclatura

--- api/src/models/authModel.js
+++ api/src/models/authModel.js
@@ -4,8 +4,8 @@ const autenticar = async (email, senha) => {
     // Consulta para autenticação de administradores
     const [userResult] = await connection.execute('SELECT tagSetor, nomeSistema, tipo, nome FROM Usuarios WHERE Email = ? AND Senha = ?', [email, senha]);
 
-    if (adminResult.length > 0) {
-        return adminResult[0];
+    if (userResult.length > 0) {
+        return userResult[0];
     }
     // Retorna `null` se nenhuma consulta encontrar correspondência
     return null;
