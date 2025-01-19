455fe7fc9d42046c9743aa450bd82915565e5884
Guilherme Cunha <alexandregui013@gmail.com>
2025-01-19 02:12:30 -0300

    Update usersModel.js

--- api/src/models/usersModel.js
+++ api/src/models/usersModel.js
@@ -65,7 +65,7 @@ const editUser = async (idUsuario, Usuario) => {
     const { tagSetor, tipo, nome, email, senha, idLider } = Usuario;
 
     const query = 'UPDATE Usuarios set tagSetor = ?, tipo = ?, nome = ?, email = ?, senha = ?, idLider = ? WHERE idUsuario = ?';
-    const [editedUser] = await connection.execute(query, [tagSetor, fotoPerfil, tipo, nome, email, senha, idLider, idUsuario]);
+    const [editedUser] = await connection.execute(query, [tagSetor, tipo, nome, email, senha, idLider, idUsuario]);
     return editedUser;
 }
 
